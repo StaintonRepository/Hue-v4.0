6a3c0524a578f9bb85fe5ac58da850ab8260aeda
Anthony Stainton <anthonystainton@gmail.com>
2022-06-28 15:20:06 -0700

    more event stuff before I leave for work.

--- src/modules/events/index.js
+++ src/modules/events/index.js
@@ -1,16 +1,33 @@
 const fs = require("fs");
 
 module.exports = (NECos) => {
-	const handlers = fs.readdirSync("./src/modules/events/handlers");
-	for(const filename of handlers){
-		const envName = filename.split(".")[0];
-		const event = require(`./handlers/${filename}`);
+	function loadEvents(){
+		const handlers = fs.readdirSync("./src/modules/events/handlers");
+		for(const filename of handlers){
+			const envName = filename.split(".")[0];
+			const event = require(`./handlers/${filename}`);
 
-		NECos.on(envName, event.bind(null, client));
+			NECos.on(envName, event.bind(null, NECos));
+		}
 	}
-
+	loadEvents();
 	// return 
 	return {
-
+		removeEvent: (name) => {
+			NECos.removeAllListeners(name);
+		},
+		loadAllEvents: loadEvents,
+		reloadEvent: (name) => {
+			const handlers = fs.readdirSync("./src/modules/events/handlers");
+			for(const filename of handlers){
+				const envName = filename.split(".")[0];
+				if(envName == name){
+					NECos.removeAllListeners(name);
+					const event = require(`./handlers/${filename}`);
+					NECos.on(envName, event.bind(null, NECos));
+					return true;
+				} else return false;
+			}
+		}
 	}
 }
