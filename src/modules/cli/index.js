4488d99910dd072b0111361c9d1529291bb511b6
Anthony Stainton <anthonystainton@gmail.com>
2022-06-29 13:37:56 -0700

    eval command line

--- src/modules/cli/index.js
+++ src/modules/cli/index.js
@@ -7,12 +7,32 @@ const rl = readline.createInterface({
 module.exports = (NECos) => {
 	const functions = {
 		run: async (cmd, args) => {
-			console.log(`Running command: ${cmd}`);
+			// temp eval cmd because I am lazy.
+			if(cmd === "eval"){
+				try{
+					let text = eval(args);
+					if (text && text.constructor.name == "Promise") text = await text;
+					if (typeof text !== "string")
+					text = require("util").inspect(text, { depth: 1 });
+
+					text = text
+						.replace(/`/g, "`" + String.fromCharCode(8203))
+						.replace(/@/g, "@" + String.fromCharCode(8203))
+						.replace(NECos.token, "ooga booga");
+
+					console.log(text);
+				}
+				catch(e){
+					NECos.Logger.error(e);
+				}
+			}
+
+
 		},
 		prompt: () => {
 			rl.question("", (answer) => {
 				const cmd = answer.split(" ")[0];
-				const args = answer.split(" ").slice(1);
+				const args = answer.split(" ").slice(1).join(" ");
 				functions.run(cmd, args);
 				functions.prompt();
 			});
