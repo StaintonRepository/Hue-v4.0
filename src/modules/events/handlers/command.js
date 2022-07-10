70e8624fa1889514772c44724b48f74b3b8394ec
Anthony Stainton <anthonystainton@gmail.com>
2022-07-10 15:19:42 -0700

    the funny

--- src/modules/events/handlers/command.js
+++ src/modules/events/handlers/command.js
@@ -6,7 +6,8 @@
 module.exports = async (client, interaction) => {
 	const command = client.Commands.cache.get(interaction.commandName);
 	console.log(command);
+	if(command.config.enabled == false) return interaction.reply("This command is currently disabled.");
 	if(command.config.adminOnly){
-		interaction.reply("Bot Admin Only");
+		return interaction.reply("Bot Admin Only");
 	} else command.run(client, interaction);
 };
