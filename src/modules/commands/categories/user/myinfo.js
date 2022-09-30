83c0988bb2d916330a3a20f252cb858b63c4218f
Anthony Stainton <anthonystainton@gmail.com>
2022-09-30 13:05:20 -0700

    test

--- src/modules/commands/categories/user/myinfo.js
+++ src/modules/commands/categories/user/myinfo.js
@@ -17,12 +17,12 @@ module.exports = {
 			.setFooter({text: client.user.username, iconURL: client.user.avatarURL(), dynmaic: true})
 			.setTitle("Database Information")
 			.setColor("GREEN")
-			.setDescription("First Apperance: <t:0>")
+			.setDescription("First Apperance: <t:0>\nLast Modified: <t:0>\nModified By: `System#0000`")
 			.setThumbnail(interaction.user.avatarURL({size: 4096, dynamic: true}))
 			.setFields([
-				{name: "Steam Information", value: "Steam Username: `Anthony`\nSteam ID: `STEAM_0:0:105215865`\nSteam ID64: `76561198170697458`\nDate Recorded: <t:0>\nLast Modified: <t:0>", inline: false},
-				{name: "Minecraft Information", value: "Minecraft Username: `ItzRock`\nMinecraft UUID: `b25cfd78-aeac-4dbc-a94c-20f54b6e9c91`\nDate Recorded: <t:0>\nLast Modified: <t:0>", inline: false},
-				{name: "Roblox Information", value: "Roblox Username: `Anthonycanada`\nRoblox ID: `66997500`\nDate Recorded: <t:0>\nLast Modified: <t:0>", inline: false},
+				{name: "Steam Information", value: "Steam Username: `Anthony`\nSteam ID: `STEAM_0:0:105215865`\nSteam ID64: `76561198170697458`\nDate Recorded: <t:0>\n", inline: false},
+				{name: "Minecraft Information", value: "Minecraft Username: `ItzRock`\nMinecraft UUID: `b25cfd78-aeac-4dbc-a94c-20f54b6e9c91`\nDate Recorded: <t:0>\n", inline: false},
+				{name: "Roblox Information", value: "Roblox Username: `Anthonycanada`\nRoblox ID: `66997500`\nDate Recorded: <t:0>\n", inline: false},
 				
 			]);
 
